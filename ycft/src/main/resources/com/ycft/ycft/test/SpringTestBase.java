c3a8555e119c00583b0f2674c00de14ce08fdb1d
郑斌 <541194741@qq.com>
2018-05-18 15:50:31 +0800

    测试

--- ycft/src/main/resources/com/ycft/ycft/test/SpringTestBase.java
+++ ycft/src/main/resources/com/ycft/ycft/test/SpringTestBase.java
@@ -8,5 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SpringTestBase extends AbstractJUnit4SpringContextTests {
 	
-	
+	public void d() {
+		
+	}
 }
