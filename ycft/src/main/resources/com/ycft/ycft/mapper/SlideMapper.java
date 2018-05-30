8ac6dbf691ff8f79d9a46d94f3fe19af74802ed1
郑斌 <541194741@qq.com>
2018-05-30 10:52:33 +0800

    主页

--- ycft/src/main/resources/com/ycft/ycft/mapper/SlideMapper.java
+++ ycft/src/main/resources/com/ycft/ycft/mapper/SlideMapper.java
@@ -5,9 +5,20 @@ import java.util.List;
 import com.ycft.ycft.po.Slide;
 
 public interface SlideMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Slide record);
+
+    int insertSelective(Slide record);
+
+    Slide selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Slide record);
+
+    int updateByPrimaryKeyWithBLOBs(Slide record);
+
+    int updateByPrimaryKey(Slide record);
 	
 	public List<Slide> selectAll();
 	//²éÑ¯»ù´¡×Ö¶Î
-	
-     
 }
